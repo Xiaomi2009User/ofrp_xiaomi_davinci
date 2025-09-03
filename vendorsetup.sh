5607c764b8f375134972e09bb5bf09a9fd9941cf
Xiaomi2009User <xiaomi2009user@gmail.com>
2025-09-03 11:22:15 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,10 +4,24 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2019-2020 The OrangeFox Recovery Project
+#	
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+# 	
+# 	Please maintain this if you use this script or any part of it
 
-add_lunch_combo omni_davinci-user
-add_lunch_combo omni_davinci-userdebug
-add_lunch_combo omni_davinci-eng
 FDEVICE="davinci"
 
 fox_get_target_device() {
